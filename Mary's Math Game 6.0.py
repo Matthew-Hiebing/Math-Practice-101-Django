860c89420e3b3222fc844f9e292adbcd717c10ec
Hiebs915 <Matthew.Hiebing@gmail.com>
2020-10-13 15:37:06 -0500

    Update Mary's Math Game 6.0.py

--- Mary's Math Game 6.0.py
+++ Mary's Math Game 6.0.py
@@ -3,20 +3,26 @@
 import random #Imports the random module for generating random integers
 import operator #Imports the operator module for using the operators:add, sub, truediv, mul
 
-operatorChoices = {"+":operator.add, "-":operator.sub, "/":operator.truediv, "*":operator.mul}
+operatorChoices = {
+	"+":operator.add,
+	"-":operator.sub,
+	"/":operator.truediv,
+	"*":operator.mul
+}
+
 op = random.choice(["+", "*", "/", "-"])
 num1 = random.randint(0,12)
 num2 = random.randint(1, 12)
 result = operatorChoices[op](num1,num2)
 guessesTaken = 0 #Program starts with 0 guesses, each time you get an incorrect answer it adds "1" to this count until you reach "2", then you're out of guesses and the program ends.
 
-print("Hello brave Math soldier, welcome to Math 101. In this game you will be presented a math problem that you will need to solve. \nIt will be a random choice of addition, and multiplication. You will have 2 guesses for each problem, after that, GAMEOVER!. \nHere's your first problem: ")
+print("Hello brave student, welcome to Math 101. In this game you will be presented a random math problem that you will need to solve.  It will be a random choice of addition, subtraction, multiplication, and division. You will have 2 guesses for each problem, after that, GAMEOVER!. \nHere's your first problem: ")
 
-while guessesTaken <= 2: #The while loop will continue to work until guessesTaken is <2.
-	if op == "+": #If the random op is "+" it runs through this if loop.
+while guessesTaken <= 2: #The while loop will continue to work until guessesTaken is <=2.
+	if op == "+": #If the operator is "+" it runs through the loop below.
 		print(str(num1), op, str(num2))
 		answer = int(input())
-		if answer == result: # If the user answers corectly it allows you to keep play.
+		if answer == result: # If the user answers correctly it allows you to keep play.
 			print("\nCorrect!  Do you want to play again? Please type Yes or No.")
 			answer = str(input())
 			if (answer == "No") or (answer == "no") or (answer == "N") or (answer == "n"): # If user says "no" the game will end.
@@ -37,10 +43,9 @@ while guessesTaken <= 2: #The while loop will continue to work until guessesTake
 						answer = str(input())
 						if (answer == "No") or (answer == "no") or (answer == "N") or (answer == "n"):
 							break
-						if (answer == "Yes") or (answer == "yes") or (answer == "Y") or (answer == "y"): # If the user answers correctly and wants to play again it generates
-																										 # a random op and integers and starts you back from the beginning.
+						if (answer == "Yes") or (answer == "yes") or (answer == "Y") or (answer == "y"): # If the user answers correctly and wants to play again it generates a random op and integers and starts you back from the beginning.
 							import random   # This last loop of re-importing random operators and integers ensures you don't get the same numbers when you go back to the beginning of the while loop.
-							import operator 
+							import operator
 							operatorChoices = {"+":operator.add, "-":operator.sub, "/":operator.truediv, "*":operator.mul}
 							op = random.choice(["+", "*", "/", "-"])
 							num1 = random.randint(1,12)
@@ -105,12 +110,12 @@ while guessesTaken <= 2: #The while loop will continue to work until guessesTake
 							num2 = random.choice([1, 2, 3, 4, 5, 6, 7, 8, 9 , 10, 11, 12])
 							result = operatorChoices[op](num1,num2)
 							continue
-		if answer != result: # If the user does not answer correctly it adds a guess to guessesTaken and returns you to the beginning.  
-							 # If you don't aswer correctly it asks you to re-answer the same poroblem you got wrong, the script only re-generates new integers if you get the answer correct.
+		if answer != result: # If the user does not answer correctly it adds a guess to guessesTaken and returns you to the beginning.
+							 # If you don't answer correctly it asks you to re-answer the same problem you got wrong, the script only re-generates new integers if you get the answer correct.
 			guessesTaken += 1
 			print("\nWrong, try again if you have guesses left.")
 
-	if op == "-": #If the random op is "-" it runs through this if loop.
+	if op == "-": #If the operator is "-" it runs through the loop below.
 		num1, num2 = sorted((random.randint(1,12), random.randint(1,12)), reverse=True)
 		result = num1 - num2
 		print(str(num1), op, str(num2))
@@ -297,7 +302,7 @@ while guessesTaken <= 2: #The while loop will continue to work until guessesTake
 			guessesTaken += 1
 			print("\nWrong, try again if you have guesses left.")
 
-	if op == "/": #If the random op is "/" it runs through this if loop.
+	if op == "/": #If the random operator is "/" it runs through the loop below.
 		import random
 		num1 = random.randint(0,12)
 		num2 = random.choice([1, 2, 3, 4, 5, 6, 7, 8, 9 , 10, 11, 12])
