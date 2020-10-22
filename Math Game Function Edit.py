7da890cc0e95a0c3ff4c5efbd0f2f0eb815dde66
Hiebs915 <Matthew.Hiebing@gmail.com>
2020-10-22 16:33:53 -0500

    Created Multiplication Function
    Created multiplication function that asks the user multiplication problems.  If they guess incorrectly two times the game ends.

---
+++ Math Game Function Edit.py
@@ -0,0 +1,33 @@
+def multiplication():
+
+    import random
+    count = 0
+
+    while True:
+        num1 = random.randint(0, 12)
+        num2 = random.randint(0, 12)
+        problemResult = num1 * num2
+
+
+        print(str(num1), '*', str(num2))
+        userAnswer = int(input())
+
+        if userAnswer == problemResult:
+            print("\nCorrect!  Do you want to play again? Please enter yes or no.")
+            userResponse = str(input())
+            if userResponse.lower()[0] == 'n':
+                break
+            if userResponse.lower()[0] == 'y':
+                continue
+
+        if userAnswer != problemResult:
+            count += 1
+            print("Sorry, that was not the correct answer.  Please try again.")
+            print(str(num1), '*', str(num2))
+            userAnswer = int(input())
+
+            if count >= 2:
+                break
+
+
+multiplication()
