23226d882705970c2f2902623f0aff8b5b67295a
Hiebs915 <Matthew.Hiebing@gmail.com>
2020-10-22 16:39:34 -0500

    Created Division Function
    Created division function that asks the user division problems. The game will end if the user takes 2 or more guesses.

--- Math Game Function Edit.py
+++ Math Game Function Edit.py
@@ -70,4 +70,42 @@ def addition():
                 break
 
 
-addition()
+#addition()
+
+
+#-----------------------------------------------------------------------------#
+
+
+def division():
+
+    import random
+    count = 0
+
+    while True:
+        num1 = random.randint(0, 12)
+        num2 = random.randint(1, 12)
+        problemResult = (num2 * num1)/num2
+
+
+        print(str(num2 * num1), '/', str(num2))
+        userAnswer = int(input())
+
+        if userAnswer == problemResult:
+            print("\nCorrect!  Do you want to play again? Please enter yes or no.")
+            userResponse = str(input())
+            if userResponse.lower()[0] == 'n':
+                break
+            if userResponse.lower()[0] == 'y':
+                continue
+
+        if userAnswer != problemResult:
+            count += 1
+            print("Sorry, that was not the correct answer.  Please try again.")
+            print(str(num1), '/', str(num2))
+            userAnswer = int(input())
+
+            if count >= 2:
+                break
+
+
+division()
