c5f950302ffb1e1d19f0963b447c2f48885d2624
Hiebs915 <Matthew.Hiebing@gmail.com>
2020-10-23 08:59:47 -0500

    Yes/No Fix  PEP 8 Update
    Fixed the results of the yes/no input after the program asks you if you want to keep playing.  Applied PEP 8 line length rules to entire script.

--- Math Game Function Edit.py
+++ Math Game Function Edit.py
@@ -1,164 +1,170 @@
 #-----------------------------------------------------------------------------#
-from random import choice
 
-#-----------------------------------------------------------------------------#
 def multiplication():
 
-    import random
-    count = 0
-
-    while True:
-        num1 = random.randint(0, 12)
-        num2 = random.randint(0, 12)
-        problemResult = num1 * num2
+    num1 = random.randint(0, 12)
+    num2 = random.randint(0, 12)
+    problemResult = num1 * num2
 
 
-        print(str(num1), '*', str(num2))
-        userAnswer = int(input())
+    print(str(num1), '*', str(num2))
+    userAnswer = int(input())
 
-        if userAnswer == problemResult:
-            print("\nCorrect!  Do you want to play again? Please enter yes or no.")
-            userResponse = str(input())
-            if userResponse.lower()[0] == 'y':
-                operators = [multiplication, division, subtraction, addition]
-                random_func = choice(operators)
-                random_func()
+    if userAnswer == problemResult:
+        print("\nCorrect!  Do you want to play again? Please enter yes or no.")
+        userResponse = str(input())
+        if userResponse.lower()[0] == 'y':
+            operators = [multiplication, division, subtraction, addition]
+            random_func = choice(operators)
+            random_func()
+        elif userResponse.lower()[0] == 'n':
+            False
+        else:
+            print("You didn't answer yes or no")
 
-            if userResponse.lower()[0] == 'n':
-                break
-
-        if userAnswer != problemResult:
+    if userAnswer != problemResult:
+        count = 0
+        while count < 2:
             count += 1
             print("Sorry, that was not the correct answer.  Please try again.")
             print(str(num1), '*', str(num2))
             userAnswer = int(input())
-
-            if count >= 2:
-                print("Sorry, you are out of guesses")
-                break
-
-
-#-----------------------------------------------------------------------------#
-def addition():
-
-    import random
-    count = 0
-
-    while True:
-        num1 = random.randint(0, 12)
-        num2 = random.randint(0, 12)
-        problemResult = num1 + num2
-
-
-        print(str(num1), '+', str(num2))
-        userAnswer = int(input())
-
-        if userAnswer == problemResult:
-            print("\nCorrect!  Do you want to play again? Please enter yes or no.")
-            userResponse = str(input())
-            if userResponse.lower()[0] == 'n':
-                break
-
-            if userResponse.lower()[0] == 'y':
+            if userAnswer == problemResult:
                 operators = [multiplication, division, subtraction, addition]
                 random_func = choice(operators)
                 random_func()
+        else:
+            print("Sorry, you are out of guesses")
 
-        if userAnswer != problemResult:
+#-----------------------------------------------------------------------------#
+
+def addition():
+
+    num1 = random.randint(0, 12)
+    num2 = random.randint(0, 12)
+    problemResult = num1 + num2
+
+
+    print(str(num1), '+', str(num2))
+    userAnswer = int(input())
+
+    if userAnswer == problemResult:
+        print("\nCorrect!  Do you want to play again? Please enter yes or no.")
+        userResponse = str(input())
+        if userResponse.lower()[0] == 'y':
+            operators = [multiplication, division, subtraction, addition]
+            random_func = choice(operators)
+            random_func()
+        elif userResponse.lower()[0] == 'n':
+            False
+        else:
+            print("You didn't answer yes or no")
+
+    if userAnswer != problemResult:
+        count = 0
+        while count < 2:
             count += 1
             print("Sorry, that was not the correct answer.  Please try again.")
             print(str(num1), '+', str(num2))
             userAnswer = int(input())
-
-            if count >= 2:
-                break
-
+            if userAnswer == problemResult:
+                operators = [multiplication, division, subtraction, addition]
+                random_func = choice(operators)
+                random_func()
+        else:
+            print("Sorry, you are out of guesses")
 
 #-----------------------------------------------------------------------------#
+
 def division():
 
-    import random
-    count = 0
-
-    while True:
-        num1 = random.randint(0, 12)
-        num2 = random.randint(1, 12)
-        problemResult = (num2 * num1)/num2
+    num1 = random.randint(0, 12)
+    num2 = random.randint(1, 12)
+    problemResult = (num2 * num1)/num2
 
 
-        print(str(num2 * num1), '/', str(num2))
-        userAnswer = int(input())
+    print(str(num2 * num1), '/', str(num2))
+    userAnswer = int(input())
 
-        if userAnswer == problemResult:
-            print("\nCorrect!  Do you want to play again? Please enter yes or no.")
-            userResponse = str(input())
-            if userResponse.lower()[0] == 'y':
+    if userAnswer == problemResult:
+        print("\nCorrect!  Do you want to play again? Please enter yes or no.")
+        userResponse = str(input())
+        if userResponse.lower()[0] == 'y':
+            operators = [multiplication, division, subtraction, addition]
+            random_func = choice(operators)
+            random_func()
+        elif userResponse.lower()[0] == 'n':
+            False
+        else:
+            print("You didn't answer yes or no")
+
+    if userAnswer != problemResult:
+        count = 0
+        while count < 2:
+            count += 1
+            print("Sorry, that was not the correct answer.  Please try again.")
+            print(str(num2 * num1), '/', str(num2))
+            userAnswer = int(input())
+            if userAnswer == problemResult:
                 operators = [multiplication, division, subtraction, addition]
                 random_func = choice(operators)
                 random_func()
-
-            if userResponse.lower()[0] == 'n':
-                break
-
-        if userAnswer != problemResult:
-            count += 1
-            print("Sorry, that was not the correct answer.  Please try again.")
-            print(str(num1), '/', str(num2))
-            userAnswer = int(input())
-
-            if count >= 2:
-                break
-
+        else:
+            print("Sorry, you are out of guesses")
 
 #-----------------------------------------------------------------------------#
+
 def subtraction():
 
-    import random
-    count = 0
-
-    while True:
-        num1, num2 = sorted((random.randint(0, 12), random.randint(0, 12)), reverse=True)
-        problemResult = (num2 * num1) - num2
+    num1,num2 =sorted((random.randint(0,12),random.randint(0,12)),reverse=True)
+    problemResult = (num2 * num1) - num2
 
 
-        print(str(num2 * num1), '-', str(num2))
-        userAnswer = int(input())
+    print(str(num2 * num1), '-', str(num2))
+    userAnswer = int(input())
 
-        if userAnswer == problemResult:
-            print("\nCorrect!  Do you want to play again? Please enter yes or no.")
-            userResponse = str(input())
-            if userResponse.lower()[0] == 'y':
+    if userAnswer == problemResult:
+        print("\nCorrect!  Do you want to play again? Please enter yes or no.")
+        userResponse = str(input())
+        if userResponse.lower()[0] == 'y':
+            operators = [multiplication, division, subtraction, addition]
+            random_func = choice(operators)
+            random_func()
+        elif userResponse.lower()[0] == 'n':
+            False
+        else:
+            print("You didn't answer yes or no")
+
+    if userAnswer != problemResult:
+        count = 0
+        while count < 2:
+            count += 1
+            print("Sorry, that was not the correct answer.  Please try again.")
+            print(str(num2 * num1), '-', str(num2))
+            userAnswer = int(input())
+            if userAnswer == problemResult:
                 operators = [multiplication, division, subtraction, addition]
                 random_func = choice(operators)
                 random_func()
-
-            if userResponse.lower()[0] == 'n':
-                break
-
-        if userAnswer != problemResult:
-            count += 1
-            print("Sorry, that was not the correct answer.  Please try again.")
-            print(str(num1), '-', str(num2))
-            userAnswer = int(input())
-
-            if count >= 2:
-                break
-
+        else:
+            print("Sorry, you are out of guesses")
 
 #-------------------------------GAME START------------------------------------#
-print("Hello brave student, welcome to Math 101. In this game you will"
-" be presented a random math problem that you will need to solve.  It"
-" will be a random choice of subtraction, addition, division, and"
-" multiplication. You will have 2 guesses per problem, after that,"
-" GAMEOVER!")
 
+print("Hello brave student, welcome to Math 101. In this game you will be"
+" presented a random math problem that you will need to solve.  It will be a"
+" random choice of subtraction, addition, division, and multiplication. You"
+" will have 2 guesses per problem, after that, GAMEOVER!")
+
+from random import choice
+import random
 operators = [multiplication, division, subtraction, addition]
 random_func = choice(operators)
-
 
 if input("\nAre you ready to start? ").lower()[0] == 'y':
     random_func()
 else:
+    pirnt("All right, another time then!")
     False
 
 
