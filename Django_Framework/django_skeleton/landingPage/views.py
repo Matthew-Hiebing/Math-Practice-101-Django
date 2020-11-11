ee2f8f4b6d5cc48498c2679c249d9e3d2c2fbfd2
Hiebs915 <Matthew.Hiebing@gmail.com>
2020-11-11 15:33:54 -0600

    Landing Page Greeting
    Created landing page greeting welcoming students to the math game.

--- Django_Framework/django_skeleton/landingPage/views.py
+++ Django_Framework/django_skeleton/landingPage/views.py
@@ -3,4 +3,8 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello World")
+    my_dict = {'welcome_message': "Hello brave student, welcome to Math 101. In this game you will be"
+               " presented a random math problem that you will need to solve.  It will be a"
+               " random choice of subtraction, addition, division, and multiplication. You"
+               " will have 2 guesses per problem, after that, GAMEOVER!"}  # Template we want to use shown as the dictionary key.
+    return render(request,'index.html',context=my_dict)
