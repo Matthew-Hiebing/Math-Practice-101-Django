b6f2a417b1ed88b679fdb63181508612707199df
Hiebs915 <Matthew.Hiebing@gmail.com>
2020-11-12 14:42:22 -0600

    Renamed Landing Page HTML Files
    Renamed landing page HTML file from index.html to main.html for better readability.

--- Django/landingPageApp/views.py
+++ Django/landingPageApp/views.py
@@ -8,6 +8,5 @@ def index(request):
                " random choice of subtraction, addition, division, and multiplication. You"
                " will have 2 guesses per problem, after that, GAMEOVER!"}  # Template we want to use shown as the dictionary key.
 
-    return render(request,'landingPageHTML/index.html',context=my_dict) #landingPage is referring to the "landingPage" folder in the "static" folder.
-
+    return render(request,'landingPageHTML/main.html',context=my_dict) #"landingPage" is referring to the landingPage folder in the static folder.
 
