d5e0d903ebf9097ce38cbb3fec3a2192b2fbd577
Hiebs915 <Matthew.Hiebing@gmail.com>
2020-11-12 16:59:49 -0600

    Landing Page Layout Changed

--- Django/django_skeleton/urls.py
+++ Django/django_skeleton/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from landingPageApp import views
 from django.conf.urls import include
+from landingPageApp import views
+
 
 urlpatterns = [
-    path('',views.index,name='index'),
+    path('',views.index,name='main-view'),
     path('landingPageApp/',include('landingPageApp.urls')),
     path('admin/', admin.site.urls),
 ]
--- Django/landingPageApp/urls.py
+++ Django/landingPageApp/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from landingPageApp import views
 
 urlpatterns = [
-    path('',views.index,name='index'),
+    # path('', views.index, name='main-view'),
+    path('', views.index, name='main-view'),
 ]
