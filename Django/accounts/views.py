db37a6e371a2df1e591b803dd95c9ce570e19758
Hiebs915 <Matthew.Hiebing@gmail.com>
2020-11-18 15:26:48 -0600

    Added Score class and code that fills in user info when new user is made.

--- Django/accounts/views.py
+++ Django/accounts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, logout
 from homepage import views
+from game.models import Score
 
 # Super User Information:
 # User Matthew.Hiebing
@@ -14,7 +15,10 @@ def signup_view(request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
-            # log in the user
+            # Create a blank_score row for each user when they make an account
+            new_game_score = Score(user=user, number_of_correct_answers=0, number_of_incorrect_answers=0, total_questions_answered=0)
+            new_game_score.save()
+            # Log the user in
             login(request, user)
             return redirect(views.homepage)
     else:
@@ -26,7 +30,7 @@ def login_view(request):
     if request.method == 'POST':
         form = AuthenticationForm(data=request.POST)
         if form.is_valid():
-            # log in the user
+            # Log the user in
             user = form.get_user()
             login(request, user)
             if 'next' in request.POST:
--- Django/game/models.py
+++ Django/game/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Score(models.Model):
+  """
+    ===
+    Scoring System
+    ===
+    This class creates a row for each new user than contains their user_id,
+    number of correctly answered problems, number of incorrectly answered
+    problems, and the total number of problems they attempted.
+  """
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+  number_of_correct_answers = models.IntegerField(null=True)
+  number_of_incorrect_answers = models.IntegerField(null=True)
+  total_questions_answered = models.IntegerField(null=False)
