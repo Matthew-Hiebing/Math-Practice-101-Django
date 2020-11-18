e3af56e60dcae8d82a09297455d44ae40f1b8820
Hiebs915 <Matthew.Hiebing@gmail.com>
2020-11-18 14:53:49 -0600

    Modified Password Requirments and base_layout.html
    Relaxed the password requirments and put a text section in that shows who's logged in.

--- Django/django_skeleton/settings.py
+++ Django/django_skeleton/settings.py
@@ -99,9 +99,9 @@ PASSWORD_HASHERS = [
 ]
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
+    # {
+    #     'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    # },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
         'OPTIONS':{'min_length':9}
@@ -109,9 +109,9 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
     },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+    # {
+    #     'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    # },
 ]
 
 
