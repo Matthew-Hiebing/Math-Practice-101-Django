f65112d3dbd86030d99872c93bdadad4dd89ba44
Hiebs915 <Matthew.Hiebing@gmail.com>
2020-11-19 08:53:58 -0600

    New Records and Requirments File
    Created new models class called "Records".  This class records information about the user and the game characteristics while the game is being played.
    
    A new requirments file was created to document what packages are needed to run this project.

--- Django/game/models.py
+++ Django/game/models.py
@@ -5,15 +5,30 @@ from django.contrib.auth.models import User
 
 
 class Score(models.Model):
-  """
+    """
     ===
     Scoring System
     ===
     This class creates a row for each new user that contains their user_id,
     number of correctly answered problems, number of incorrectly answered
     problems, and the total number of problems they attempted.
-  """
-  user = models.ForeignKey(User, on_delete=models.CASCADE)
-  number_of_correct_answers = models.IntegerField(null=True)
-  number_of_incorrect_answers = models.IntegerField(null=True)
-  total_questions_answered = models.IntegerField(null=False)
+    """
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    number_of_incorrect_answers = models.IntegerField(null=True)
+    total_questions_answered = models.IntegerField(null=False)
+
+
+class Records(models.Model):
+    """
+    ===
+    User Gaming Records
+    ===
+    This class records the characteristics of the game while the user is
+    playing.  It records the date/time for each problem, the problem they were
+    given, their answer, and the true answer.
+    """
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    math_problem = models.CharField(null=False)
+    date_time = models.DateTimeField(null=False)
+    user_answer = models.IntegerField(null=False,max_length=3)
+    answer = models.IntegerField(null=False)
