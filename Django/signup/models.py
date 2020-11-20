a106941c074a4a403ae7ab1d522478b9f1666889
Hiebs915 <Matthew.Hiebing@gmail.com>
2020-11-20 17:13:45 -0600

    Removed old signup app functionality

--- Django/signup/models.py
+++ Django/signup/models.py
@@ -1,12 +1,1 @@
-from django.db import models
 
-# Super User Information:
-# User Matthew.Hiebing
-# Email: Matthew.Hiebing@gmail.com
-# Password: testpass1029
-
-# Create your models here.
-class User(models.Model):
-    first_name = models.CharField(max_length=128)
-    last_name = models.CharField(max_length=128)
-    email = models.EmailField(max_length=254, unique=True)
--- Django/signup/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import include, path
-from signup import views
-
-urlpatterns = [
-    path('',views.users, name='signup'),
-]
