1658f0ddac278915caf23cbcd34c6ad5866ff7d5
Hiebs915 <Matthew.Hiebing@gmail.com>
2020-11-28 11:25:15 -0600

    testing javascript

---
+++ Django/static/js/game_logic.js
@@ -0,0 +1,10 @@
+function addition() {
+    var num1 = Math.random(0,12)
+    var num2 = Math.random(0,12)
+    var problemResult = num1 + num2
+    document.getElementById("game").innerHTML = problemResult
+}
+
+function random() {
+    number = Math.random(0,12)
+}
