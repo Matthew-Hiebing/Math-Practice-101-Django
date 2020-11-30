27c441ea037648628c3789ed878e3554159309b6
Hiebs915 <Matthew.Hiebing@gmail.com>
2020-11-30 17:54:20 -0600

    Worked on incoporating JS on game page

--- Django/static/js/game_logic.js
+++ Django/static/js/game_logic.js
@@ -2,4 +2,7 @@ function addition() {
     let num1 = Math.floor(Math.random() * 13);
     let num2 = Math.floor(Math.random() * 13);
     let problemResult = num1 + num2;
+    alert(num1 + num2)
 }
+
+addition()
