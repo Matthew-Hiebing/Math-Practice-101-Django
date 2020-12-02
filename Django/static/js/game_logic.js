053536b157073e3c142c8285dd5a9ea979cfbb44
Hiebs915 <Matthew.Hiebing@gmail.com>
2020-12-02 11:19:58 -0600

    Fixed Jquery error, js file function

--- Django/static/js/game_logic.js
+++ Django/static/js/game_logic.js
@@ -1,7 +1,8 @@
-function addition() {
+const btn = document.querySelector('#TESTID');
+
+btn.onclick = function(addition) {
     let num1 = Math.floor(Math.random() * 13);
     let num2 = Math.floor(Math.random() * 13);
     let problemResult = num1 + num2;
-    alert(num1 + num2)
+    console.log(problemResult);
 }
-addition()
