5a25957af0f065610bf36975f77b32cb864508f4
Hiebs915 <Matthew.Hiebing@gmail.com>
2020-12-02 14:47:01 -0600

    displayed parts of function results in HTML

--- Django/static/js/game_logic.js
+++ Django/static/js/game_logic.js
@@ -4,5 +4,8 @@ btn.addEventListener('click', function(addition) {
     let num1 = Math.floor(Math.random() * 13);
     let num2 = Math.floor(Math.random() * 13);
     let problemResult = num1 + num2;
+    // let problem = (num1,'+',num2,'=')
     console.log(num1,'+',num2,'=',problemResult)
+    document.getElementById('num1').textContent = (num1, '+', num2, '=', problemResult)
 })
+
