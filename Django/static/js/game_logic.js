d957ca358834c647dfafbb7999f53d8b231e116d
Hiebs915 <Matthew.Hiebing@gmail.com>
2020-12-02 11:29:19 -0600

    Able to print math problem in console
    Was able to print math problem in console using JS.

--- Django/static/js/game_logic.js
+++ Django/static/js/game_logic.js
@@ -1,8 +1,8 @@
 const btn = document.querySelector('#TESTID');
 
-btn.onclick = function(addition) {
+btn.addEventListener('click', function(addition) {
     let num1 = Math.floor(Math.random() * 13);
     let num2 = Math.floor(Math.random() * 13);
     let problemResult = num1 + num2;
-    console.log(problemResult);
-}
+    console.log(num1,'+',num2,'=',problemResult)
+})
