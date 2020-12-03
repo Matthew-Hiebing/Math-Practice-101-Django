9fc6b37b97815db669fa349606ccd29de41105f5
Hiebs915 <Matthew.Hiebing@gmail.com>
2020-12-03 11:27:54 -0600

    Commented Out Python Code for Game Logic
    Commented out Python code for game logic because we are using JS instead of Python views to generate the math problems.  Had to modify game.urls.py so the game page would show up and not call the previous Python function.  Updated game.views.py and created a new function that only displays the HTML.

--- Django/game/views.py
+++ Django/game/views.py
@@ -3,9 +3,13 @@ from django.contrib.auth.decorators import login_required
 import random
 
 # Create your views here.
-@login_required(login_url="/accounts/login/")
-def game_logic(request):
-    num1 = random.randint(0, 12)
-    num2 = random.randint(0, 12)
-    problem_result = num1 * num2
-    return render(request, 'game/game.html', {"problem_result": problem_result})
+# @login_required(login_url="/accounts/login/")
+# def game_logic(request):
+#     num1 = random.randint(0, 12)
+#     num2 = random.randint(0, 12)
+#     problem_result = num1 * num2
+#     return render(request, 'game/game.html', {"problem_result": problem_result})
+
+
+def game(request):
+    return render(request, 'game/game.html')
