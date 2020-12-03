21d2be0ec2004034150ce390ad7522158b51dc5b
Hiebs915 <Matthew.Hiebing@gmail.com>
2020-12-02 18:02:18 -0600

    Button Update
    "Press here for your first problem" button now calls a random function from the array of functions.

--- Django/static/js/game_logic.js
+++ Django/static/js/game_logic.js
@@ -1,35 +1,51 @@
-const btn = document.querySelector('#START');
+const btn = document.querySelector('#START')
 
-btn.addEventListener('click', function(addition) {
-    let num1 = Math.floor(Math.random() * 13);
-    let num2 = Math.floor(Math.random() * 13);
-    let problemResult = num1 + num2;
-    console.log(num1,'+',num2,'=',problemResult)
-    document.getElementById('mathProblem').innerHTML = (`${num1} + ${num2} =`)
-})
+const randomFunc = [
+    multiplication,
+    division,
+    addition,
+    subtraction,
+]
 
-btn.addEventListener('click', function (multiplication) {
+btn.addEventListener(
+    'click',
+    function () {
+        randomFunc[Math.floor(Math.random() * randomFunc.length)]();
+    }
+)
+
+function multiplication() {
     let num1 = Math.floor(Math.random() * 13);
     let num2 = Math.floor(Math.random() * 13);
     let problemResult = num1 * num2;
-    console.log(num1, '*', num2, '=', problemResult)
-    document.getElementById('mathProblem').innerHTML = (`${num1} * ${num2} =`)
-})
+    console.log(num1, '*', num2, '=', problemResult);
+    document.getElementById('mathProblem').innerHTML = (`${num1} * ${num2} =`);
+}
 
-btn.addEventListener('click', function (division) {
+function division() {
     let num1 = Math.floor(Math.random() * 13);
     let num2 = Math.floor(Math.random() * 12) + 1;
-    let problemResult = (num2 * num1) / num2;
-    console.log(num1, '/', num2, '=', problemResult)
-    document.getElementById('mathProblem').innerHTML = (`${num1} / ${num2} =`)
-})
+    let problemResult = (num1 * num2) / num2;
+    console.log(num1 * num2, '/', num2, '=', problemResult);
+    document.getElementById('mathProblem').innerHTML = (`${num1 * num2} / ${num2} =`);
+}
 
-btn.addEventListener('click', function (subtraction) {
-    let num1= Math.floor(Math.random() * 13)
-    let num2 = Math.floor(Math.random() * 13)
-    let numList = [num1,num2]
+function addition() {
+    let num1 = Math.floor(Math.random() * 13);
+    let num2 = Math.floor(Math.random() * 13);
+    let problemResult = num1 + num2;
+    console.log(num1,'+',num2,'=',problemResult);
+    document.getElementById('mathProblem').innerHTML = (`${num1} + ${num2} =`);
+}
+
+function subtraction() {
+    let num1= Math.floor(Math.random() * 13);
+    let num2 = Math.floor(Math.random() * 13);
+    let numList = [num1,num2];
     numList.sort(function(a,b){return b-a});
-    let problemResult = numList[0] - numList[1]
-    console.log(numList[0], '-', numList[1], '=', problemResult)
-    document.getElementById('mathProblem').innerHTML = (`${numList[0]} - ${numList[1]} =`)
-})
+    let problemResult = numList[0] - numList[1];
+    console.log(numList[0], '-', numList[1], '=', problemResult);
+    document.getElementById('mathProblem').innerHTML =
+    (`${numList[0]} - ${numList[1]} =`);
+}
+
