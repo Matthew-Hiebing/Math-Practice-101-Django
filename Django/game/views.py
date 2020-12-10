38048ff862dfaec8fb2ddeb2ea86f9c7920635b1
Hiebs915 <Matthew.Hiebing@gmail.com>
2020-12-10 09:48:40 -0600

    feat: Changed New Button ID
    Changed New Button ID from 'start' to 'New_Problem_Button'.

--- Django/game/views.py
+++ Django/game/views.py
@@ -72,7 +72,7 @@ def game(request):
 
 
 @login_required(login_url='/accounts/login/')
-# @csrf_exempt
+@csrf_exempt
 def submit_score_details(request):
     '''
     Submits math problem details, the user's answer, and tally's the math
