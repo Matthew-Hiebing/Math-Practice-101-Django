8f62fba496a2868dbc4c9694f1ed71b938303c0f
Hiebs915 <Matthew.Hiebing@gmail.com>
2020-12-11 19:22:35 -0600

    refactor: Refactored JS
    Used functions to refactor most of the JS file.

--- Django/static/js/game_logic.js
+++ Django/static/js/game_logic.js
@@ -1,5 +1,14 @@
-//----------------------------Splash Screen Check----------------------------//
+//-------------------------------Variables-----------------------------------//
+const randomFunc = [multiplication, popUpision, addition, subtraction,]
 const checkBox = document.querySelector('#splash_screen_preference_check_box');
+const newProblemBtn = document.querySelector('#new_problem_button');
+const input_form = document.getElementById('input_form');
+const checkButton = document.querySelector('#result_check');
+const popUp = document.getElementById('check_button_alert');
+let problem, userInput, correctAnswer, questionStatus;
+
+
+//-------------------Splash Screen Check & Axios Request---------------------//
 if (checkBox) {
     checkBox.addEventListener('change', function () {
         console.log(checkBox.checked)
@@ -11,48 +20,29 @@ if (checkBox) {
 }
 
 
-//---------------------------------Constants---------------------------------//
-const newProblemBtn = document.querySelector('#new_problem_button');
-const input_form = document.getElementById('input_form');
-const checkButton = document.querySelector('#result_check');
-const popUp = document.getElementById('check_button_alert');
-
-
 //---------------------------Check Button Actions----------------------------//
-let problem, userInput, correctAnswer, questionStatus;
 checkButton.addEventListener('click', function () {
     problem = document.querySelector('#math_problem').innerText
     userInput = document.querySelector('#user_input').value
     correctAnswer = document.querySelector('#correct_answer').value
 
     if (document.querySelector('#user_input').value === "") {
-        console.log('No input from user, alert shown')
-        popUp.style.visibility = 'visible'
+        noAnswerPrompts();
 
     } else if (document.querySelector('#correct_answer').getAttribute
     ('value') === document.querySelector('#user_input').value) {
-        checkButton.classList.remove('btn-primary','btn-lg','btn-danger');
-        checkButton.classList.add('btn-success');
-        checkButton.textContent = 'Correct!';
-        questionStatus = 'Correct'
-        popUp.style.visibility = 'hidden'
-        document.getElementById('new_problem_button').disabled = false;
-        document.getElementById('result_check').disabled = true;
-        sendMathResults()
+        correctAnswerPrompts();
+        sendMathResults();
 
     } else if (document.querySelector('#correct_answer').getAttribute
     ('value') !== document.querySelector('#user_input').value) {
-        checkButton.classList.remove('btn-primary','btn-lg');
-        checkButton.classList.add('btn-danger');
-        checkButton.textContent = 'Incorrect';
-        questionStatus = 'Incorrect'
-        popUp.style.visibility = 'hidden'
-        document.getElementById('new_problem_button').disabled = false;
-        document.getElementById('result_check').disabled = true;
-        sendMathResults()
+        incorrectAnswerPrompts();
+        sendMathResults();
     }
 });
 
+
+//---------------------------------Functions---------------------------------//
 function sendMathResults() {
     axios.defaults.xsrfCookieName = 'csrftoken';
     axios.defaults.xsrfHeaderName = 'X-CSRFToken';
@@ -62,8 +52,84 @@ function sendMathResults() {
         "true_answer": correctAnswer,
         "question_status": questionStatus,
     });
-    console.log(`Problem:${problem}, User Input: ${userInput},
+    console.log(`Problem: ${problem}, User Input: ${userInput},
         Correct Answer: ${correctAnswer}, Question Status: ${questionStatus}`);
+}
+
+
+function noAnswerPrompts() {
+    console.log('No input from user, alert shown')
+    popUp.style.visibility = 'visible'
+}
+
+
+function incorrectAnswerPrompts() {
+    checkButton.classList.remove('btn-primary', 'btn-lg');
+    checkButton.classList.add('btn-danger');
+    checkButton.textContent = 'Incorrect';
+    questionStatus = 'Incorrect'
+    popUp.style.visibility = 'hidden'
+    document.getElementById('new_problem_button').disabled = false;
+    document.getElementById('result_check').disabled = true;
+}
+
+
+function correctAnswerPrompts() {
+    checkButton.classList.remove('btn-primary', 'btn-lg', 'btn-danger');
+    checkButton.classList.add('btn-success');
+    checkButton.textContent = 'Correct!';
+    questionStatus = 'Correct'
+    popUp.style.visibility = 'hidden'
+    document.getElementById('new_problem_button').disabled = false;
+    document.getElementById('result_check').disabled = true;
+}
+
+
+function multiplication() {
+    let num1 = Math.floor(Math.random() * 13);
+    let num2 = Math.floor(Math.random() * 13);
+    let problemResult = num1 * num2;
+    console.log(num1, '*', num2, '=', problemResult);
+    document.getElementById('math_problem').innerHTML =
+        (`${num1} x ${num2}`);
+    return problemResult
+}
+
+
+function popUpision() {
+    let num1 = Math.floor(Math.random() * 13);
+    let num2 = Math.floor(Math.random() * 12) + 1;
+    let problemResult = (num1 * num2) / num2;
+    console.log(num1 * num2, '/', num2, '=', problemResult);
+    document.getElementById('math_problem').innerHTML =
+        (`${num1 * num2} ÷ ${num2}`);
+    return problemResult
+}
+
+
+function addition() {
+    let num1 = Math.floor(Math.random() * 13);
+    let num2 = Math.floor(Math.random() * 13);
+    let problemResult = num1 + num2;
+    console.log(num1, '+', num2, '=', problemResult);
+    document.getElementById('math_problem').innerHTML =
+        (`${num1} + ${num2}`);
+    return problemResult
+}
+
+
+function subtraction() {
+    let num1 = Math.floor(Math.random() * 13);
+    let num2 = Math.floor(Math.random() * 13);
+    let numList = [num1, num2];
+    numList.sort(function (a, b) {
+        return a - b
+    });
+    let problemResult = numList[1] - numList[0];
+    console.log(numList[1], '-', numList[0], '=', problemResult);
+    document.getElementById('math_problem').innerHTML =
+        (`${numList[1]} - ${numList[0]}`);
+    return problemResult
 }
 
 
@@ -87,57 +153,7 @@ newProblemBtn.addEventListener('click', function () {
     }
 });
 
-const randomFunc = [
-    multiplication,
-    popUpision,
-    addition,
-    subtraction,
-]
-
-function multiplication() {
-    let num1 = Math.floor(Math.random() * 13);
-    let num2 = Math.floor(Math.random() * 13);
-    let problemResult = num1 * num2;
-    console.log(num1, '*', num2, '=', problemResult);
-    document.getElementById('math_problem').innerHTML =
-    (`${num1} x ${num2}`);
-    return problemResult
-}
 
 
-function popUpision() {
-    let num1 = Math.floor(Math.random() * 13);
-    let num2 = Math.floor(Math.random() * 12) + 1;
-    let problemResult = (num1 * num2) / num2;
-    console.log(num1 * num2, '/', num2, '=', problemResult);
-    document.getElementById('math_problem').innerHTML =
-    (`${num1 * num2} ÷ ${num2}`);
-    return problemResult
-}
 
-
-function addition() {
-    let num1 = Math.floor(Math.random() * 13);
-    let num2 = Math.floor(Math.random() * 13);
-    let problemResult = num1 + num2;
-    console.log(num1,'+',num2,'=',problemResult);
-    document.getElementById('math_problem').innerHTML =
-    (`${num1} + ${num2}`);
-    return problemResult
-}
-
-
-function subtraction() {
-    let num1 = Math.floor(Math.random() * 13);
-    let num2 = Math.floor(Math.random() * 13);
-    let numList = [num1, num2];
-    numList.sort(function (a, b) {
-        return a - b
-    });
-    let problemResult = numList[1] - numList[0];
-    console.log(numList[1], '-', numList[0], '=', problemResult);
-    document.getElementById('math_problem').innerHTML =
-    (`${numList[1]} - ${numList[0]}`);
-    return problemResult
-}
 
