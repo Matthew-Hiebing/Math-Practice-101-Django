050d1ac16856b15ff68970e194801508e809fa24
Hiebs915 <Matthew.Hiebing@gmail.com>
2020-12-14 14:21:50 -0600

    feat: Moved Chart JS to Separate File
    Moved chart JS to separate JS file and used variables to define Django templates.

---
+++ Django/static/js/scores_chart.js
@@ -0,0 +1,39 @@
+var correct_answer_count = document.getElementById("correct_answer_count").value;
+var incorrect_answer_count = document.getElementById("incorrect_answer_count").value;
+var total_questions_answered = document.getElementById("total_questions_answered").value;
+
+var ctx = document.getElementById('myChart').getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Total Correct', 'Total Incorrect', 'Total Answered'],
+        datasets: [{
+            label: 'Math Results',
+            data: [
+                correct_answer_count,
+                incorrect_answer_count,
+                total_questions_answered
+            ],
+        backgroundColor: [
+            'rgba(13, 222, 2, 0.2)',
+            'rgba(250, 0, 0, 0.2)',
+            'rgba(10, 38, 255, 0.2)',
+        ],
+        borderColor: [
+            'rgba(7, 158, 0, 1)',
+            'rgba(225, 0, 0, 1)',
+            'rgba(30, 56, 255, 1)',
+        ],
+            borderWidth: 1.5
+        }]
+    },
+options: {
+    scales: {
+        yAxes: [{
+            ticks: {
+                beginAtZero: true
+            }
+        }]
+    }
+}
+    });
