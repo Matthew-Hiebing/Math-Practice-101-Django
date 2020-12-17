feb3ee1e9d98dd746782fcd48a8a9cad074942f1
Hiebs915 <Matthew.Hiebing@gmail.com>
2020-12-17 11:06:52 -0600

    feat: Updated Settings.py

--- django_skeleton/settings.py
+++ django_skeleton/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -137,3 +138,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     STATIC_DIR,
 ]
+
+# Configure Django App for Heroku.
+django_heroku.settings(locals())
