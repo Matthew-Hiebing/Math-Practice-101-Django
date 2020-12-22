e585b3927ab94c971993b6b4ae668d2dcd683329
Hiebs915 <Matthew.Hiebing@gmail.com>
2020-12-22 09:27:39 -0600

    fix: Fixed Chart Sizing on Mobile
    The bar graphs were not displaying properly on mobile.

--- static/js/game_logic.js
+++ static/js/game_logic.js
@@ -242,7 +242,7 @@ function multiplication() {
     let problemResult = num1 * num2;
     console.log(num1, '*', num2, '=', problemResult);
     document.getElementById('math_problem').innerHTML =
-        (`${num1} x ${num2}`);
+        (`${num1}x${num2}`);
     return problemResult
 }
 
@@ -253,7 +253,7 @@ function division() {
     let problemResult = (num1 * num2) / num2;
     console.log(num1 * num2, '/', num2, '=', problemResult);
     document.getElementById('math_problem').innerHTML =
-        (`${num1 * num2} ÷ ${num2}`);
+        (`${num1*num2} ÷ ${num2}`);
     return problemResult
 }
 
@@ -264,7 +264,7 @@ function addition() {
     let problemResult = num1 + num2;
     console.log(num1, '+', num2, '=', problemResult);
     document.getElementById('math_problem').innerHTML =
-        (`${num1} + ${num2}`);
+        (`${num1}+${num2}`);
     return problemResult
 }
 
@@ -279,6 +279,6 @@ function subtraction() {
     let problemResult = numList[1] - numList[0];
     console.log(numList[1], '-', numList[0], '=', problemResult);
     document.getElementById('math_problem').innerHTML =
-        (`${numList[1]} - ${numList[0]}`);
+        (`${numList[1]}-${numList[0]}`);
     return problemResult
 }
