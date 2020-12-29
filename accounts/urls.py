aa4a9eb6baab1ce9bc3d79a666e16997126cb06a
Hiebs915 <Matthew.Hiebing@gmail.com>
2020-12-29 13:40:33 -0600

    refactor: Changed signup page view from signup_view to UserCreate.as_view()

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,7 +4,8 @@ from accounts import views
 app_name = 'accounts'
 
 urlpatterns = [
-    path('signup/', views.signup_view, name='signup'),
+    path('signup/', views.UserCreate.as_view(), name='signup'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
+
 ]
