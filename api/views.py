757e512ae0beef007d66fd2db78004c003a65591
Hiebs915 <Matthew.Hiebing@gmail.com>
2021-01-06 14:26:02 -0600

    docs: Updated def tally_results docs.

--- api/views.py
+++ api/views.py
@@ -57,9 +57,10 @@ class SubmitScoreDetails(APIView):
     @staticmethod
     def tally_results(request):
         '''
-        Identifies the current user, counts the number of 'Correct' and 'Incorrect'
-        answers, and sums the total questions answered for the current user. Once
-        this information is tally'd, the results are updated in the Scores class.
+        Identifies the current user, counts the number of 'Correct' and
+        'Incorrect' answers, and sums the total questions answered for the
+        current user. Once this information is tally'd, the results are updated
+        in the Scores class.
         '''
         correct_answer_count = request.user.record_set.all().filter(
             question_status='Correct').count()
@@ -115,4 +116,6 @@ class RequestScoreDetails(APIView):
         return Response(data={
             'correct_answer_count': score.number_of_correct_answers,
             'incorrect_answer_count': score.number_of_incorrect_answers,
-            'total_questions_answered': score.total_questions_answered}, status=status.HTTP_200_OK)
+            'total_questions_answered': score.total_questions_answered},
+            status=status.HTTP_200_OK
+        )
